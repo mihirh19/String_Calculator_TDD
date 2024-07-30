23a9cf191267be9bef8c3a3cb5d0226bb619bd45
mihirh19 <miheerhadvani990@gmail.com>
2024-07-30 19:39:18 +0530

    for new line delimiter it fails test cases

--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -14,3 +14,18 @@ def test_add_two_numbers():
    cal = string_calculator()
    assert cal.add("1,2") == 3
    assert cal.add("5,4") == 9
+
+def test_add_three_numbers():
+   cal = string_calculator()
+   assert cal.add("1,2,3") == 6
+   assert cal.add("5,4,3") == 12
+
+def test_add_many_numbers():
+   cal = string_calculator()
+   assert cal.add("1,2,3,4,5") == 15
+   assert cal.add("5,4,3,2,1") == 15
+   
+def test_add_newline_delimiter():
+   cal = string_calculator()
+   assert cal.add("1\n2,3") == 6
+   assert cal.add("5\n4\n3") == 12
