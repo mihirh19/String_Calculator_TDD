013ca589dd57ba22dd7875a0149a09630e11994a
mihirh19 <miheerhadvani990@gmail.com>
2024-07-30 20:54:12 +0530

    add comments and docs for the source and test files

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,13 +1,27 @@
 import re
 class string_calculator:
    def add(self, numbers : str) -> int:
+      """Add numbers in a string
+      
+      Arguments:
+      string : numbers -- string containing numbers separated by commas or new lines or custom delimiters
+      Return: int -- sum of the numbers in the string
+      """
+      
+      # Check if the string is empty
       if not numbers:
          return 0
+      
+      # initialize the delimiter
       delimiter = ','
-
+      
+      
+      # Check if the string starts with a custom delimiter
       if numbers.startswith("//"):
+         # Split the string into delimiter and numbers on the first new line
          delimiter_part, numbers = numbers.split("\n", 1)
          delimiter = delimiter_part[2:]
+         
          if delimiter == "":
             delimiter = "\n"
       
@@ -15,9 +29,13 @@ class string_calculator:
       if delimiter != "\n":
          numbers = numbers.replace("\n", delimiter)
       
+      
+      # Split the numbers into a list
       integers_to_add= re.split(re.escape(delimiter), numbers)
       integers = []
       invalid_inputs =[]
+      
+      # Check if the numbers are valid integers
       for number in integers_to_add.copy():
          number = number.strip()
          if number == '':
@@ -27,12 +45,20 @@ class string_calculator:
          else:
             invalid_inputs.append(number)
       integers_to_add = integers
+      
+      # Raise an error if there are invalid inputs
       if invalid_inputs:
          raise ValueError(f"invalid input: {', '.join(invalid_inputs)}")
       
       
+      
+      # list of negative numbers
       negative_numbers = [number for number in integers_to_add if number < 0]
 
+      
+      # Raise an error if there are negative numbers
       if negative_numbers:
          raise ValueError(f"negative numbers not allowed: {', '.join(map(str, negative_numbers))}")
+      
+      # return the sum of the numbers
       return sum(integers_to_add)
--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -1,47 +1,58 @@
 import pytest
 from string_calculator import string_calculator
 
+
+
 def test_add_empty_string():
+   """Test  case for empty string returns 0."""
    calc = string_calculator()
    assert calc.add("") == 0 
 
 def test_add_single_number():
+   """Test case for single number returns the number."""
    cal = string_calculator()
    assert cal.add("1") == 1
    assert cal.add("2") == 2
 
 def test_add_two_numbers():
+   """Test case for two numbers returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("1,2") == 3
    assert cal.add("5,4") == 9
 
 def test_add_three_numbers():
+   """Test case for three numbers returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("1,2,3") == 6
    assert cal.add("5,4,3") == 12
 
 def test_add_many_numbers():
+   """Test case for many numbers returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("1,2,3,4,5") == 15
    assert cal.add("5,4,3,2,1") == 15
    
 def test_add_newline_delimiter():
+   """Test case for new line delimiter returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("1\n2,3") == 6
    assert cal.add("5\n4\n3") == 12
 
 def test_add_custom_delimiter():
+   """Test case for custom delimiter returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("//;\n1;2") == 3
    assert cal.add("//;\n5;4\n3") == 12
 
 def test_add_custom_delimiter2():
+   """Test case for custom delimiter with length > 1  returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("//|\n1|2") == 3
    assert cal.add("//|\n5|4\n3") == 12
    assert cal.add("//**\n5**4\n3**2") == 14
 
 def test_add_negative_numbers():
+   """Test case for negative numbers raises an error."""
    cal = string_calculator()
    with pytest.raises(ValueError, match="negative numbers not allowed: -1"):
       cal.add("-1")
@@ -49,10 +60,12 @@ def test_add_negative_numbers():
       cal.add('-1,-3,-4,6')
 
 def test_add_custom_delimiter_with_newline():
+   """Test case for custom delimiter with newline returns the sum of the numbers."""
    cal = string_calculator()
    assert cal.add("//\n\n1\n2\n3") == 6
 
 def test_non_numeric_characters():
+   """Test case for non-numeric characters raises an error."""
    cal = string_calculator()
    with pytest.raises(ValueError, match="invalid input: a"):
       cal.add("1,a")
