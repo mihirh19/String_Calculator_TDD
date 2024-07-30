21a962b6480aac2676504764498b23a8823025f0
mihirh19 <miheerhadvani990@gmail.com>
2024-07-30 19:42:04 +0530

    add logic for handle the new line delimiter and it passes the test case

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -4,7 +4,7 @@ class string_calculator:
       if not numbers:
          return 0
       
-      integers_to_add= re.split(',', numbers)
+      integers_to_add= re.split(r',|\n', numbers)
       integers_to_add= list(map(int, integers_to_add))
 
       return sum(integers_to_add)
