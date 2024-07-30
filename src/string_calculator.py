c9a951c0f893614c65d6dced5c95519b56526f28
mihirh19 <miheerhadvani990@gmail.com>
2024-07-30 19:26:52 +0530

    check for empty string

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,3 +1,4 @@
 class string_calculator:
-   def add(self, numbers):
-      pass
+   def add(self, numbers : str) -> int:
+      if not numbers:
+         return 0
--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -1,1 +1,6 @@
 import pytest
+from string_calculator import string_calculator
+
+def test_add_empty_string():
+   calc = string_calculator()
+   assert calc.add("") == 0 
