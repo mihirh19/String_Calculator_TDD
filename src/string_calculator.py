93fa0611209e5b64c62322818f859be06e76e7ca
mihirh19 <miheerhadvani990@gmail.com>
2024-07-30 19:54:57 +0530

    add the logic for custom delimiter and it passes the test cases

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -3,8 +3,13 @@ class string_calculator:
    def add(self, numbers : str) -> int:
       if not numbers:
          return 0
-      
-      integers_to_add= re.split(r',|\n', numbers)
+      delimiter = ','
+
+      if numbers.startswith("//"):
+         delimiter, numbers = numbers[2:].split("\n", 1)
+
+      numbers = numbers.replace('\n', delimiter)
+      integers_to_add= re.split(re.escape(delimiter), numbers)
       integers_to_add= list(map(int, integers_to_add))
 
       return sum(integers_to_add)
