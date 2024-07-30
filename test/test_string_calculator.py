c97dce387df27d5f2d7e3e9979440fcb577ff354
mihirh19 <miheerhadvani990@gmail.com>
2024-07-30 19:27:56 +0530

    add test case for 1 number and it fails

--- test/test_string_calculator.py
+++ test/test_string_calculator.py
@@ -4,3 +4,8 @@ from string_calculator import string_calculator
 def test_add_empty_string():
    calc = string_calculator()
    assert calc.add("") == 0 
+
+def test_add_single_number():
+   cal = string_calculator()
+   assert cal.add("1") == 1
+   assert cal.add("2") == 2
