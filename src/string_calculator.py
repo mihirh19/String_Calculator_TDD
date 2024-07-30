a42c8974be501c0536d3b9ba2573236f344a38c2
mihirh19 <miheerhadvani990@gmail.com>
2024-07-31 00:31:07 +0530

    Refactor string_calculator class methods added separate methods for each task

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -8,13 +8,42 @@ class string_calculator:
       Return: int -- sum of the numbers in the string
       """
       
+      
       # Check if the string is empty
       if not numbers:
          return 0
       
+      
       # initialize the delimiter
       delimiter = ','
       
+      
+      # split the string into delimiter and numbers
+      delimiter , numbers = self.__find_delimiter(numbers)
+      
+      
+      # convert the string to a list of integers
+      integers_to_add = self.__convert_string_to_integers(numbers, delimiter)
+      
+      
+      
+      # check if there are negative numbers in the list
+      self.__check_negative_numbers(integers_to_add)
+      
+      
+      
+      # return the sum of the numbers
+      return sum(integers_to_add)
+   
+   def __find_delimiter(self, numbers : str) -> list:
+      """Find the delimiter in the string
+      
+      Arguments:
+      string : numbers -- string containing numbers separated by commas or new lines or custom delimiters
+      Return: ist -- list containing delimiter and numbers
+      """
+      
+      delimiter=','
       
       # Check if the string starts with a custom delimiter
       if numbers.startswith("//"):
@@ -24,16 +53,28 @@ class string_calculator:
          
          if delimiter == "":
             delimiter = "\n"
+      else:
+         delimiter = ','
+      
+      return [delimiter, numbers]
+
+   def __convert_string_to_integers(self, numbers, delimiter = ',') -> list:
+      """Convert the string to a list of integers
+      
+      Arguments:
+      string : integers_to_add -- list of strings
+      Return: list -- list of integers
+      """
+      
       
       # Replace new lines with delimiters
       if delimiter != "\n":
          numbers = numbers.replace("\n", delimiter)
       
-      
       # Split the numbers into a list
       integers_to_add= re.split(re.escape(delimiter), numbers)
       integers = []
-      invalid_inputs =[]
+      invalid_inputs = []
       
       # Check if the numbers are valid integers
       for number in integers_to_add.copy():
@@ -45,12 +86,21 @@ class string_calculator:
          else:
             invalid_inputs.append(number)
       integers_to_add = integers
+   
       
       # Raise an error if there are invalid inputs
       if invalid_inputs:
          raise ValueError(f"invalid input: {', '.join(invalid_inputs)}")
       
+      return integers_to_add
+   
+   
+   def __check_negative_numbers(self, integers_to_add : list):
+      """Check if there are negative numbers in the list
       
+      Arguments:
+      list : integers_to_add -- list of integers
+      """
       
       # list of negative numbers
       negative_numbers = [number for number in integers_to_add if number < 0]
@@ -59,6 +109,3 @@ class string_calculator:
       # Raise an error if there are negative numbers
       if negative_numbers:
          raise ValueError(f"negative numbers not allowed: {', '.join(map(str, negative_numbers))}")
-      
-      # return the sum of the numbers
-      return sum(integers_to_add)
